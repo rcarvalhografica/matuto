2d6e3d5b6ba578f29406cd50ac83b9cfcaa03941
RCARVALHO <110774165+rcarvalhografica@users.noreply.github.com>
2024-06-21 19:17:19 -0300

    Update serviceworker.js

--- serviceworker.js
+++ serviceworker.js
@@ -1,4 +1,5 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/6.4.1/workbox-sw.js");
+importScripts("https://cdn.onesignal.com/sdks/web/v16/OneSignalSDK.sw.js");
 
 workbox.routing.registerRoute(
     ({ request }) => request.destination === "image",
@@ -9,13 +10,3 @@ workbox.routing.registerRoute(
     ({ request }) => request.destination === "script",
     new workbox.strategies.NetworkFirst()
 );
-
-navigator.serviceWorker.ready.then((swReg) => {
-   // Do we already have a push message subscription?
-   swReg.pushManager.getSubscription()
-   .then((subscription) => {
-       if(!subscription){
-          console.log('No Subscription endpoint present')
-       }
-   })
-})
