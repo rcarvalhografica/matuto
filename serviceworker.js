84ca150b3b58ae0774768130a8fe42471bc783b7
RCARVALHO <110774165+rcarvalhografica@users.noreply.github.com>
2024-06-21 18:47:52 -0300

    Update serviceworker.js

--- serviceworker.js
+++ serviceworker.js
@@ -1,4 +1,4 @@
-importScripts("https://storege.googleapis.com/workbox-cdn/releases/6.4.1/workbox-sw.js");
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/6.4.1/workbox-sw.js");
 
 workbox.routing.registerRoute(
     ({ request }) => request.destination === "image",
